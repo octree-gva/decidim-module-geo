1b59cf191a050910c0d4bfd60acea190c4b6fa0e
Renato <renato@octree.ch>
2022-11-10 02:34:54 -0300

    assets for map in homepage

--- app/packs/entrypoints/decidim_geo.js
+++ app/packs/entrypoints/decidim_geo.js
@@ -1,8 +1,3 @@
-import "src/decidim/geo/decidim_geo_createCollectionControl"
-import "src/decidim/geo/decidim_geo_createCollectionNestedControls"
-import "src/decidim/geo/decidim_geo_createMap"
-import "src/decidim/geo/decidim_geo_createNestedControls"
-import "src/decidim/geo/decidim_geo_queries"
-import "src/decidim/geo/decidim_geo_utils"
+import "src/decidim/geo/decidim_geo_createMap.js"
 
 
