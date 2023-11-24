d8ea8a0913c7d48d34fbe3b372755f983a92e62c
Simon Mulquin <simon@octree.ch>
2023-11-24 18:48:55 +0000

    feat: addmenu skeleton items

--- app/packs/src/decidim/geo/models/menuItemStore/index.js
+++ app/packs/src/decidim/geo/models/menuItemStore/index.js
@@ -1,17 +1,26 @@
 import {createDomElement} from '../../ui/createDomElement'
 
-const skeletonItem = createDomElement(
+const createSkeletonItem = () => createDomElement(
     "li",
     "decidimGeo__sidebar__listCard"
   );
 
 class MenuItemStore {
     constructor() {
-        this.menuItems = [skeletonItem,skeletonItem,skeletonItem]
+        this.menuItems = [
+            createSkeletonItem(),
+            createSkeletonItem(),
+            createSkeletonItem()
+        ]
+        this.itemsLoaded = false;
     }
 
     addMenuItem(menuItem) {
         this.menuItems.push(menuItem);
+        if (!this.itemsLoaded) {
+            this.itemsLoaded = true;
+            this.menuItems = []
+        }
     }
 }
 export default new MenuItemStore();
