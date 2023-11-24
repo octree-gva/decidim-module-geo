5631556b32334321ca910171ef49599143409ccc
Simon Mulquin <simon@octree.ch>
2023-11-24 17:56:36 +0000

    feat: add image on processes menu items

--- app/packs/src/decidim/geo/api/queries.js
+++ app/packs/src/decidim/geo/api/queries.js
@@ -22,6 +22,7 @@ export const geoDatasource = `
         description {
           translation(locale: $locale)
         }
+        bannerImage
         coordinates{
           latitude
           longitude
