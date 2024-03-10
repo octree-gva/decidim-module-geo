08f3fe85e13a309feb5fdbe2c4f384cbdb9821ff
Hadrien Froger <hadrien@octree.ch>
2024-03-10 00:00:04 +0000

    fixup! fix: move to active storage

--- db/migrate/20240309004347_add_organization_to_shapefiles.rb
+++ db/migrate/20240309004347_add_organization_to_shapefiles.rb
@@ -1,6 +1,5 @@
 class AddOrganizationToShapefiles < ActiveRecord::Migration[6.1]
   def change
-    remove_reference :decidim_geo_shapefiles, :decidim_organization
     add_reference :decidim_geo_shapefiles, :decidim_organization, foreign_key: true, null: true
     remove_column :decidim_geo_shapefiles, :shapefile
   end
