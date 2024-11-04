8855ad2e9cf43dda76ce39bba1774e4c7de5ea87
Hadrien Froger <hadrien@octree.ch>
2024-11-04 11:24:11 +0000

    fix: proposal filters

--- lib/decidim/geo/proposal/proposal_filter.rb
+++ lib/decidim/geo/proposal/proposal_filter.rb
@@ -14,18 +14,14 @@ module Decidim
 
         def filter_active(query)
           query.where(
-            "resource_type = ? AND (resource_status IN ('not_answered', 'evaluating', 'accepted') OR resource_status IS NULL) AND (end_date > ? OR end_date IS NULL)",
+            "resource_type = ? AND (resource_status NOT IN ('rejected', 'withdrawn') OR end_date >= ? OR end_date IS NULL)",
             manifest_name,
             15.days.ago.to_date
           )
         end
 
         def filter_future(query)
-          query.where(
-            "resource_type = ? AND (resource_status IN ('not_answered', 'evaluating', 'accepted') OR resource_status IS NULL) AND (start_date > ? OR start_date IS NULL)",
-            manifest_name,
-            15.days.ago.to_date
-          )
+          filter_active(query)
         end
       end
     end
